e85ef0e46c605eb1bb53830e4b60c5272cc7f4b1
MinjiY <upi907@naver.com>
2025-05-25 19:34:55 +0900

    test: Http 상태 코드에 따른 사용자 지정 html 테스트

--- src/test/java/Response/ResponseTest.java
+++ src/test/java/Response/ResponseTest.java
@@ -1,11 +1,11 @@
-package Request;
+package Response;
 
 
 import com.was.HttpResponse;
 import com.was.HttpStatus;
-import com.was.config.ServerConfig;
 
-import org.junit.Before;
+import com.was.config.ServerConfig;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.*;
@@ -16,24 +16,23 @@ import static org.junit.Assert.assertEquals;
 
 
 public class ResponseTest {
-    private String testDirectory = "src/test/resources/";
-    private ServerConfig serverConfig;
+    private static String testDirectory = "src/test/resources/";
+    private static ServerConfig serverConfig;
 
-    @Before
-    public void setUp() throws Exception {
-
-        ServerConfig.load(Paths.get(testDirectory+"config/ServerConfig.json"));
+    @BeforeClass
+    public static void setUp() throws Exception {
+        ServerConfig.load(Paths.get("src/test/resources/config/ServerConfig.json"));
         serverConfig = ServerConfig.getInstance();
     }
 
     @Test
     public void test_Response_403_error() throws IOException {
-        String file = "Response/HTTP_403.html";
-        HttpResponse response = new HttpResponse(new BufferedOutputStream(createOutputStream(file)));
+        String outDirectory = "Response/HTTP_403.html";
+        HttpResponse response = new HttpResponse(new BufferedOutputStream(createOutputStream(testDirectory, outDirectory)));
         response.setStatus(HttpStatus.FORBIDDEN);
         response.response(testDirectory, serverConfig.getVirtualHosts().get("_default").getErrorPage());
 
-        String expectedPath = testDirectory+file; // 윗줄에서 response를 통해 새로 생성된 파일
+        String expectedPath = testDirectory+outDirectory; // 윗줄에서 response를 통해 새로 생성된 파일
         String expectedString = Files.readString(Paths.get(expectedPath));
 
         // 응답을 직접 파일에 기록했으므로 split필요함, 응답 헤더와 바디는 \r\n\r\n 로 구분됨
@@ -48,12 +47,12 @@ public class ResponseTest {
 
     @Test
     public void test_Response_404_error() throws IOException {
-        String file = "Response/HTTP_404.html";
-        HttpResponse response = new HttpResponse(new BufferedOutputStream(createOutputStream(file)));
+        String outDirectory = "Response/HTTP_404.html";
+        HttpResponse response = new HttpResponse(new BufferedOutputStream(createOutputStream(testDirectory, outDirectory)));
         response.setStatus(HttpStatus.NOT_FOUND);
         response.response(testDirectory, serverConfig.getVirtualHosts().get("_default").getErrorPage());
 
-        String expectedPath = testDirectory+file; // 윗줄에서 response를 통해 새로 생성된 파일
+        String expectedPath = testDirectory+outDirectory; // 윗줄에서 response를 통해 새로 생성된 파일
         String expectedString = Files.readString(Paths.get(expectedPath));
 
         // 응답을 직접 파일에 기록했으므로 split필요함, 응답 헤더와 바디는 \r\n\r\n 로 구분됨
@@ -67,12 +66,12 @@ public class ResponseTest {
 
     @Test
     public void test_Response_500_error() throws IOException {
-        String file = "Response/HTTP_500.html";
-        HttpResponse response = new HttpResponse(new BufferedOutputStream(createOutputStream(file)));
+        String outDirectory = "Response/HTTP_500.html";
+        HttpResponse response = new HttpResponse(new BufferedOutputStream(createOutputStream(testDirectory, outDirectory)));
         response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
         response.response(testDirectory, serverConfig.getVirtualHosts().get("_default").getErrorPage());
 
-        String expectedPath = testDirectory+file; // 윗줄에서 response를 통해 새로 생성된 파일
+        String expectedPath = testDirectory+outDirectory; // 윗줄에서 response를 통해 새로 생성된 파일
         String expectedString = Files.readString(Paths.get(expectedPath));
 
         // 응답을 직접 파일에 기록했으므로 split필요함, 응답 헤더와 바디는 \r\n\r\n 로 구분됨
@@ -84,7 +83,7 @@ public class ResponseTest {
         assertEquals(body, actualString);
     }
 
-    private OutputStream createOutputStream(String filename) throws FileNotFoundException {
-        return new FileOutputStream(new File(testDirectory + filename));
+    private OutputStream createOutputStream(String dir, String filename) throws FileNotFoundException {
+        return new FileOutputStream(new File(dir + filename));
     }
 }
