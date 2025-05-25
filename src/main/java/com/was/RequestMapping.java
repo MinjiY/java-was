f912aebeee872bfeb649a7d6a69fe4d809c09a36
MinjiY <upi907@naver.com>
2025-05-25 19:39:38 +0900

    mod: 클래스로딩에 필요한 변수명 수정

--- src/main/java/com/was/RequestMapping.java
+++ src/main/java/com/was/RequestMapping.java
@@ -1,5 +1,6 @@
 package com.was;
 
+import com.was.config.ServerConfig;
 import com.was.exception.ExceptionCode;
 import com.was.exception.ResourceNotFoundException;
 import com.was.servlet.KSTTime;
@@ -13,9 +14,11 @@ import java.util.concurrent.ConcurrentHashMap;
 public class RequestMapping {
     private static final Logger logger = LoggerFactory.getLogger(RequestMapping.class);
     public static Map<String, SimpleServlet> servletMap = new ConcurrentHashMap<>();
+    public static ServerConfig serverConfig;
 
     static {
         servletMap.put("/KSTTime" , new KSTTime());
+        serverConfig = ServerConfig.getInstance();
     }
 
 
@@ -38,10 +41,9 @@ public class RequestMapping {
     private static SimpleServlet loadServletDynamically(String uri){
         // '/' 제거 후 '.' 기준으로 패키지와 클래스 분리
         String cleanedUri = uri.startsWith("/") ? uri.substring(1) : uri;
-        String className = "com.was";
-        // 클래스 로딩
+        String rootPackage = "com.was";
         try {
-            Class<?> clazz = Class.forName(className + "." + cleanedUri);
+            Class<?> clazz = Class.forName(rootPackage + "." + cleanedUri);
             return (SimpleServlet) clazz.getDeclaredConstructor().newInstance();
         }catch (Exception e) {
             throw new ResourceNotFoundException(ExceptionCode.RESOURCE_NOT_FOUND);
